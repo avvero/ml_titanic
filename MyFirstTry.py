76255dca2a2dd9e8d96e08de51205a1c8e6cf2ef
Belyaev Anton Yurevich <fxdev-belyaev-ay@fxclub.org>
2017-01-11 15:07:35 +0700

    first

--- MyFirstTry.py
+++ MyFirstTry.py
@@ -3,8 +3,34 @@ import pandas as pd
 import pylab as P
 import matplotlib.pyplot as plt
 
-#Print you can execute arbitrary python code
+# Print you can execute arbitrary python code
 train = pd.read_csv("train.csv", dtype={"Age": np.float64}, )
 test = pd.read_csv("test.csv", dtype={"Age": np.float64}, )
+
+# Print to standard output, and see the results in the "log" section below after running your script
+print("\n\nLen of the training data: " + str(len(train)))
+print("\n\nLen of the test data: " + str(len(test)))
+print("\n\nTypes of the data: ")
+print(train.dtypes)
+print("\n\nTop of the training data:")
+print(train.head())
+print("\n\nSummary statistics of training data")
+print(train.describe())
+
+# train['Age'].hist()
+# P.show()
+
+# The titanic variable is available here.
+train["Age"] = train["Age"].fillna(train["Age"].median())
+
+# Replace all the occurences of male with the number 0.
+train.loc[train["Sex"] == "male", "Sex"] = 0
+train.loc[train["Sex"] == "female", "Sex"] = 1
+
+train["Embarked"] = train["Embarked"].fillna("S")
+
+train.loc[train["Embarked"] == "S", "Embarked"] = 0
+train.loc[train["Embarked"] == "C", "Embarked"] = 1
+train.loc[train["Embarked"] == "Q", "Embarked"] = 2
 
 print(train.head())
