404d201340d5e86bff823864cd980090fb4e3537
avvero <avvero@mail.ru>
2017-01-16 21:39:54 +0700

    V Score Plot

--- MyFirstTry.py
+++ MyFirstTry.py
@@ -3,6 +3,11 @@ import pandas as pd
 from sklearn import cross_validation
 from sklearn import linear_model
 from data_preporation import prepare
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+from titanic_support import plot_learning_curve
+import matplotlib.pyplot as plt
 
 
 # Print you can execute arbitrary python code
@@ -26,11 +31,30 @@ prepare(train)
 
 # Train 1
 predictors = ['Pclass', 'Sex', 'Age', 'Fare', 'Embarked', 'SibSp', 'Parch', 'FamilySize', 'NameLength', 'Title']
+
+polynomial_features = PolynomialFeatures(degree=1, include_bias=False)
 alg = linear_model.LogisticRegression()
-scores = cross_validation.cross_val_score(alg, train[predictors], train["Survived"], cv=3)
+pipeline = Pipeline([("polynomial_features", polynomial_features),
+                     ("logistic_regression", alg)])
+scores = cross_val_score(
+    pipeline,
+    train[predictors],
+    train["Survived"],
+    cv=3,
+    # scoring="neg_mean_squared_error"
+)
 
 print("Scores")
+print(scores)
+print(scores)
 print(scores.mean())
+
+print("Alg")
+print(alg)
+
+print("Plot")
+plot_learning_curve(pipeline, "sdf", train[predictors], train["Survived"], (0.7, 1.01), cv=3, n_jobs=1)
+plt.show()
 
 print("-------TEST--------")
 
